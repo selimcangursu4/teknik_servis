7d19e5c96c945c4b2c873a4efc16ab0ab4f83d4d
Selim <selimcangursu@yandex.com>
2025-02-05 21:46:11 +0300

    template partials ayrıldı

--- teknik_servis/resources/views/partials/sidebar.blade.php
+++ teknik_servis/resources/views/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <nav id="sidebar" class="sidebar js-sidebar">
     <div class="sidebar-content js-simplebar">
       <a class="sidebar-brand" href="index.html">
-        <span class="align-middle">AdminKit</span>
+        <span class="align-middle">Teknik Servis CRM</span>
       </a>
       <ul class="sidebar-nav">
         <li class="sidebar-header"> Pages </li>
