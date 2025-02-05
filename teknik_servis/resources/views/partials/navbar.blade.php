be9d1f1c159be1f7b54617693615031af5a13a5f
Selim <selimcangursu@yandex.com>
2025-02-05 22:10:15 +0300

    Sidebar ve Header Menüleri Güncellendi.

--- teknik_servis/resources/views/partials/navbar.blade.php
+++ teknik_servis/resources/views/partials/navbar.blade.php
@@ -12,7 +12,7 @@
           </div>
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0" aria-labelledby="alertsDropdown">
-          <div class="dropdown-menu-header"> 4 New Notifications </div>
+          <div class="dropdown-menu-header"> 4 Yeni Bildirim </div>
           <div class="list-group">
             <a href="#" class="list-group-item">
               <div class="row g-0 align-items-center">
@@ -26,44 +26,9 @@
                 </div>
               </div>
             </a>
-            <a href="#" class="list-group-item">
-              <div class="row g-0 align-items-center">
-                <div class="col-2">
-                  <i class="text-warning" data-feather="bell"></i>
-                </div>
-                <div class="col-10">
-                  <div class="text-dark">Lorem ipsum</div>
-                  <div class="text-muted small mt-1">Aliquam ex eros, imperdiet vulputate hendrerit et.</div>
-                  <div class="text-muted small mt-1">2h ago</div>
-                </div>
-              </div>
-            </a>
-            <a href="#" class="list-group-item">
-              <div class="row g-0 align-items-center">
-                <div class="col-2">
-                  <i class="text-primary" data-feather="home"></i>
-                </div>
-                <div class="col-10">
-                  <div class="text-dark">Login from 192.186.1.8</div>
-                  <div class="text-muted small mt-1">5h ago</div>
-                </div>
-              </div>
-            </a>
-            <a href="#" class="list-group-item">
-              <div class="row g-0 align-items-center">
-                <div class="col-2">
-                  <i class="text-success" data-feather="user-plus"></i>
-                </div>
-                <div class="col-10">
-                  <div class="text-dark">New connection</div>
-                  <div class="text-muted small mt-1">Christina accepted your request.</div>
-                  <div class="text-muted small mt-1">14h ago</div>
-                </div>
-              </div>
-            </a>
           </div>
           <div class="dropdown-menu-footer">
-            <a href="#" class="text-muted">Show all notifications</a>
+            <a href="#" class="text-muted">Tüm Bildirimleri Göster</a>
           </div>
         </div>
       </li>
@@ -75,7 +40,7 @@
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0" aria-labelledby="messagesDropdown">
           <div class="dropdown-menu-header">
-            <div class="position-relative"> 4 New Messages </div>
+            <div class="position-relative"> 4 Yeni Mail </div>
           </div>
           <div class="list-group">
             <a href="#" class="list-group-item">
@@ -90,45 +55,9 @@
                 </div>
               </div>
             </a>
-            <a href="#" class="list-group-item">
-              <div class="row g-0 align-items-center">
-                <div class="col-2">
-                  <img src="img/avatars/avatar-2.jpg" class="avatar img-fluid rounded-circle" alt="William Harris">
-                </div>
-                <div class="col-10 ps-2">
-                  <div class="text-dark">William Harris</div>
-                  <div class="text-muted small mt-1">Curabitur ligula sapien euismod vitae.</div>
-                  <div class="text-muted small mt-1">2h ago</div>
-                </div>
-              </div>
-            </a>
-            <a href="#" class="list-group-item">
-              <div class="row g-0 align-items-center">
-                <div class="col-2">
-                  <img src="img/avatars/avatar-4.jpg" class="avatar img-fluid rounded-circle" alt="Christina Mason">
-                </div>
-                <div class="col-10 ps-2">
-                  <div class="text-dark">Christina Mason</div>
-                  <div class="text-muted small mt-1">Pellentesque auctor neque nec urna.</div>
-                  <div class="text-muted small mt-1">4h ago</div>
-                </div>
-              </div>
-            </a>
-            <a href="#" class="list-group-item">
-              <div class="row g-0 align-items-center">
-                <div class="col-2">
-                  <img src="img/avatars/avatar-3.jpg" class="avatar img-fluid rounded-circle" alt="Sharon Lessman">
-                </div>
-                <div class="col-10 ps-2">
-                  <div class="text-dark">Sharon Lessman</div>
-                  <div class="text-muted small mt-1">Aenean tellus metus, bibendum sed, posuere ac, mattis non.</div>
-                  <div class="text-muted small mt-1">5h ago</div>
-                </div>
-              </div>
-            </a>
           </div>
           <div class="dropdown-menu-footer">
-            <a href="#" class="text-muted">Show all messages</a>
+            <a href="#" class="text-muted">Tüm E-Postaları Göster</a>
           </div>
         </div>
       </li>
@@ -142,16 +71,14 @@
         </a>
         <div class="dropdown-menu dropdown-menu-end">
           <a class="dropdown-item" href="pages-profile.html">
-            <i class="align-middle me-1" data-feather="user"></i> Profile </a>
+            <i class="align-middle me-1" data-feather="user"></i> Profilim </a>
           <a class="dropdown-item" href="#">
-            <i class="align-middle me-1" data-feather="pie-chart"></i> Analytics </a>
+            <i class="align-middle me-1" data-feather="pie-chart"></i> Gelen Kutusu </a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="index.html">
-            <i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy </a>
           <a class="dropdown-item" href="#">
-            <i class="align-middle me-1" data-feather="help-circle"></i> Help Center </a>
+            <i class="align-middle me-1" data-feather="help-circle"></i> Destek </a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Log out</a>
+          <a class="dropdown-item" href="#">Güvenli Çıkış</a>
         </div>
       </li>
     </ul>
